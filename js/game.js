681be9398b6472a6730e461ce0665a60634225d6
Ketan Chowdhury <114414974+dexprexxtion@users.noreply.github.com>
2023-12-23 20:33:51 +0530

    Updated game.css and game.js

--- js/game.js
+++ js/game.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     const countdownContainer = document.querySelector('.countdown_container');
     const personDataContainer = document.querySelector('.person_data_container');
-    const personDataName = document.querySelector('.person_data_name')
+    const personDataName = document.querySelector('.person_data_name');
     const personDataAge = document.querySelector('.person_data_age');
 
     // Countdown function
@@ -11,36 +11,38 @@ document.addEventListener('DOMContentLoaded', function() {
             return;
         }
 
-        //Create countdown container
+        // Create countdown container
         const countdownElement = document.createElement('div');
         countdownElement.textContent = count;
         countdownElement.className = 'countdown_number';
 
-        //Append countdown element to container
+        // Append countdown element to container
         countdownContainer.appendChild(countdownElement);
 
-        //Remove countdown element after a delay
+        // Remove countdown element after a delay
         setTimeout(function() {
             countdownElement.remove();
             // Continue countdown with the next number
-            startCountdown(count -1);
+            startCountdown(count - 1);
         }, 1000);
     }
 
     // Function to generate and display name and age
     function generateNameAndAge() {
-        console.log('Generating name and age')
+        console.log('Generating name and age');
         const chance = new Chance();
         const name = chance.first({ gender: 'male' });
-        const age = chance.integer({ min: 13, max: 17});
+        const age = chance.integer({ min: 1, max: 3 });
 
         personDataName.textContent = name;
-        personDataAge.textContent = age + ' years old';
+
+        if (age === 1) {
+            personDataAge.textContent = age + ' month old';
+        } else {
+            personDataAge.textContent = age + ' months old';
+        }
     }
 
     // Start the countdown when the page loads
     startCountdown(5);
-
-
-
-})
+});
