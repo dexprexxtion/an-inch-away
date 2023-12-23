e993e865adbc5b1987509f68de1e13cf8567b020
Ketan Chowdhury <114414974+dexprexxtion@users.noreply.github.com>
2023-12-23 19:36:04 +0530

    start.html complete.

--- js/start_scripts.js
+++ js/start_scripts.js
@@ -1,13 +1,26 @@
 document.addEventListener('DOMContentLoaded', function() {
     // Get element
     const start_p1_t1 = document.querySelector('.start_p1_t1');
+    const goButton = document.querySelector('.go_button');
+    
+    // Variables
+    var born = 'false';
 
     // Use Chance to generate a random name
     const chance = new Chance();
     const name = chance.first({ gender: 'male' });
 
     //Set the name to the element
-    start_p1_t1.textContent = name;
+    if (born === 'true') {
+      start_p1_t1.textContent = name;  
+    }
+    
+    goButton.addEventListener('click', function() {
+        // Redirect or perform actions to proceed to the next part of the game
+        window.location.href = "game.html"; // Replace with the actual file/path for Chapter 2
+    });
+    
+    
 
 
 });
